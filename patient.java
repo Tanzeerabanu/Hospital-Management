0888fa5f36278cf4f683520c18d7ec660ceb8cb8
Tanzeerabanu <91175637+Tanzeerabanu@users.noreply.github.com>
2021-12-15 18:32:17 +0530

    Add files via upload

---
+++ patient.java
@@ -0,0 +1,49 @@
+package com;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="patient")
+public class patient {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int patientid;
+	public int getPatientid() {
+		return patientid;
+	}
+	public void setPatientid(int patientid) {
+		this.patientid = patientid;
+	}
+	private String name;
+	private String email;
+	private long mobile;
+	private String password;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getMobile() {
+		return mobile;
+	}
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
